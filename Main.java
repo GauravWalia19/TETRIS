741f402dcb0af2cb0f76aa5b6eac42b81913a97c
GauravWalia19 <gauravwalia019@gmail.com>
2018-10-05 11:48:16 +0530

    added all shapes

--- Main.java
+++ Main.java
@@ -12,20 +12,31 @@ import RAINBOW.*;
  *  # #
  */
 /*
- *  LSHAPE
+ *  LR SHAPE
  *  #
  *  #
  *  # #
  */
+/*
+ *  LL SHAPE
+ *    #
+ *    # 
+ *  # #
+*/
 /*
  *  TSHAPE
  *  # # #
  *    #
  */
 /*
- *  ZSHAPE
+ *  ZL SHAPE
  *  # #
  *    # #
+ */
+/*
+ *  ZR SHAPE
+ *    # #
+ *  # #
  */
 public class Main 
 {
@@ -40,26 +51,78 @@ public class Main
         Block b = new Block(2, 20);
         Block c = new Block(2, 21);
         Block d = new Block(2, 22);
-        Shape one = new Shape(a, b, c, d); // created line shape
+        Shape LINE = new Shape(a, b, c, d); // created line shape
 
+        a.setX(2);
+        a.setY(19);
+        b.setX(2);
+        b.setY(20);
+        c.setX(3);
+        c.setY(19);
+        d.setX(3);
+        d.setY(20);
         Shape SQUARE = new Shape(a,b,c,d); //SQAURE SHAPE
 
-        Shape LSHAPE = new Shape(a,b,c,d); //LSHAPE
+        a.setX(3);
+        a.setY(19);
+        b.setX(4);
+        b.setY(19);
+        c.setX(5);
+        c.setY(19);
+        d.setX(5);
+        d.setY(20);
+        Shape LRSHAPE = new Shape(a,b,c,d); //LR SHAPE
+
+        a.setX(3);
+        a.setY(19);
+        b.setX(4);
+        b.setY(19);
+        c.setX(5);
+        c.setY(19);
+        d.setX(5);
+        d.setY(18);
+        Shape LLSHAPE = new Shape(a,b,c,d); //LL SHAPE
         
+        a.setX(2);
+        a.setY(19);
+        b.setX(2);
+        b.setY(20);
+        c.setX(2);
+        c.setY(21);
+        d.setX(3);
+        d.setY(20);
         Shape TSHAPE = new Shape(a,b,c,d); //TSHAPE
 
-        Shape ZSHAPE = new Shape(a,b,c,d); //ZSHAPE
+        a.setX(2);
+        a.setY(19);
+        b.setX(2);
+        b.setY(20);
+        c.setX(3);
+        c.setY(20);
+        d.setX(3);
+        d.setY(21);
+        Shape ZLSHAPE = new Shape(a,b,c,d); //ZLSHAPE
+
+        a.setX(2);
+        a.setY(19);
+        b.setX(2);
+        b.setY(20);
+        c.setX(3);
+        c.setY(18);
+        d.setX(3);
+        d.setY(19);
+        Shape ZRSHAPE = new Shape(a,b,c,d); //ZRSHAPE
         
         //intial position of the shape
-        board.insertShape(one); //insert initial shape 
+        board.insertShape(LINE); //insert initial shape 
         board.printBoard(); //print the board
         System.out.println();
-        
+        System.exit(0);
         //start game
         while(true)
         {
             board.clearboard(); // clear board
-            if(!board.movedown(one)) //default move down
+            if(!board.movedown(LINE)) //default move down
             {
                 break;
             }
@@ -67,19 +130,19 @@ public class Main
             char ans=in.next().charAt(0);
             if(ans=='D' || ans=='d') //move right
             {
-                if(!board.moveright(one))
+                if(!board.moveright(LINE))
                 {
                     break;
                 }
             }
             else if(ans=='A' || ans=='a') //moveleft
             {
-                if(!board.moveleft(one))
+                if(!board.moveleft(LINE))
                 {
                     break;
                 }
             }
-            board.insertShape(one); //insert shape on board
+            board.insertShape(LINE); //insert shape on board
             board.printBoard(); // print board
             System.out.println();
         }
