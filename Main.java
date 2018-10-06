3a241ecca0e693dd9dbe11e8edd413190cdcf3df
GauravWalia19 <gauravwalia019@gmail.com>
2018-10-06 09:41:24 +0530

    added random shapes

--- Main.java
+++ Main.java
@@ -3,6 +3,83 @@ import java.util.*;
 //USING RAINBOW 
 import RAINBOW.*;
 
+public class Main {
+    public static void main(String[] args) 
+    {
+        Scanner in = new Scanner(System.in);
+        Board board = new Board(50, 40); // initaize the board
+        boolean flag_shape_fixed = false; // initial shape is now fixed
+        int shape_counter = 0; //for different shapes
+
+        // making the line blocks
+        Block a = new Block(2, 19);
+        Block b = new Block(2, 20);
+        Block c = new Block(2, 21);
+        Block d = new Block(2, 22);
+        Shape LINE = new Shape(a, b, c, d); // created line shape
+
+        // intial position of the shape
+        board.insertShape(LINE); // insert initial shape
+        board.printBoard(); // print the board
+        System.out.println();
+        
+        flag_shape_fixed = false;
+        // start game
+        while (true) 
+        {
+            // System.out.println(LINE);
+            board.clearboard(); // clear board
+            flag_shape_fixed = board.movedown(LINE); // default move down
+            System.out.println(shape_counter);
+            if(flag_shape_fixed) 
+            {
+                board.insertFixedShape(LINE);
+                board.printBoard();
+                //create new shape
+                if(shape_counter==6)
+                {
+                    shape_counter=0;
+                }
+                else
+                {
+                    shape_counter++;
+                }
+                LINE = create_shape(LINE, shape_counter);
+
+                flag_shape_fixed=false;
+            }
+            System.out.println("ENTER THE OPTION");
+            char ans = in.next().charAt(0);
+            if (ans == 'D' || ans == 'd') // move right
+            {
+                if (!board.moveright(LINE)) 
+                {
+                    break;
+                }
+            } 
+            else if (ans == 'A' || ans == 'a') // moveleft
+            {
+                if (!board.moveleft(LINE)) 
+                {
+                    break;
+                }
+            }
+            else if(ans=='w' || ans=='W') //rotate shape
+            {
+                
+            }
+            else if(ans=='q' || ans=='Q') //EXIT GAME
+            {
+                System.exit(0);
+            }
+            // rotate shape
+            board.insertShape(LINE); // insert shape on board
+            board.printBoard(); // print board
+            System.out.println();
+        }
+        in.close();
+    }
+
 /*
  *  LINE  
  *  # # # #
@@ -39,133 +116,91 @@ import RAINBOW.*;
  *    # #
  *  # #
  */
-public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        Board board = new Board(50, 40); // initaize the board
-        boolean flag_shape_fixed = false; // initial shape is now fixed
-
-        // making the line blocks
-        Block a = new Block(2, 19);
-        Block b = new Block(2, 20);
-        Block c = new Block(2, 21);
-        Block d = new Block(2, 22);
-        Shape LINE = new Shape(a, b, c, d); // created line shape
-
-        // a.setX(2);
-        // a.setY(19);
-        // b.setX(2);
-        // b.setY(20);
-        // c.setX(3);
-        // c.setY(19);
-        // d.setX(3);
-        // d.setY(20);
-        // Shape SQUARE = new Shape(a,b,c,d); //SQAURE SHAPE
-
-        // a.setX(3);
-        // a.setY(19);
-        // b.setX(4);
-        // b.setY(19);
-        // c.setX(5);
-        // c.setY(19);
-        // d.setX(5);
-        // d.setY(20);
-        // Shape LRSHAPE = new Shape(a,b,c,d); //LR SHAPE
-
-        // a.setX(3);
-        // a.setY(19);
-        // b.setX(4);
-        // b.setY(19);
-        // c.setX(5);
-        // c.setY(19);
-        // d.setX(5);
-        // d.setY(18);
-        // Shape LLSHAPE = new Shape(a,b,c,d); //LL SHAPE
-
-        // a.setX(2);
-        // a.setY(19);
-        // b.setX(2);
-        // b.setY(20);
-        // c.setX(2);
-        // c.setY(21);
-        // d.setX(3);
-        // d.setY(20);
-        // Shape TSHAPE = new Shape(a,b,c,d); //TSHAPE
-
-        // a.setX(2);
-        // a.setY(19);
-        // b.setX(2);
-        // b.setY(20);
-        // c.setX(3);
-        // c.setY(20);
-        // d.setX(3);
-        // d.setY(21);
-        // Shape ZLSHAPE = new Shape(a,b,c,d); //ZLSHAPE
-
-        // a.setX(2);
-        // a.setY(19);
-        // b.setX(2);
-        // b.setY(20);
-        // c.setX(3);
-        // c.setY(18);
-        // d.setX(3);
-        // d.setY(19);
-        // Shape ZRSHAPE = new Shape(a,b,c,d); //ZRSHAPE
-
-        // intial position of the shape
-        board.insertShape(LINE); // insert initial shape
-        board.printBoard(); // print the board
-        System.out.println();
-        
-        flag_shape_fixed = false;
-        // start game
-        while (true) 
+    public static Shape create_shape(Shape sh,int num)
+    {
+        switch(num)
         {
-            board.clearboard(); // clear board
-            flag_shape_fixed = board.movedown(LINE); // default move down
-            if(flag_shape_fixed) 
-            {
-                board.insertFixedShape(LINE);
-                board.printBoard();
-
-                //make new shape
-                a.setX(2);
-                a.setY(19);
-                b.setX(2);
-                b.setY(20);
-                c.setX(3);
-                c.setY(19);
-                d.setX(3);
-                d.setY(20);
-                // Shape SQUARE = new Shape(a,b,c,d); //SQAURE SHAPE
-
-                flag_shape_fixed=false;
-            }
-            System.out.println("ENTER THE OPTION");
-            char ans = in.next().charAt(0);
-            if (ans == 'D' || ans == 'd') // move right
-            {
-                if (!board.moveright(LINE)) 
-                {
-                    break;
-                }
-            } 
-            else if (ans == 'A' || ans == 'a') // moveleft
-            {
-                if (!board.moveleft(LINE)) 
-                {
-                    break;
-                }
-            }
-            else if(ans=='w' || ans=='W') //rotate shape
-            {
+            case 0:
+                //LINE SHAPE
+                sh.getarrofblock()[0].setX(2);
+                sh.getarrofblock()[0].setY(19);
+                sh.getarrofblock()[1].setX(2);
+                sh.getarrofblock()[1].setY(20);
+                sh.getarrofblock()[2].setX(2);
+                sh.getarrofblock()[2].setY(21);
+                sh.getarrofblock()[3].setX(2);
+                sh.getarrofblock()[3].setY(22);
+                break;
+            case 1:
+                //SQUARE SHAPE
+                sh.getarrofblock()[0].setX(2);
+                sh.getarrofblock()[0].setY(19);
+                sh.getarrofblock()[1].setX(2);
+                sh.getarrofblock()[1].setY(20);
+                sh.getarrofblock()[2].setX(3);
+                sh.getarrofblock()[2].setY(19);
+                sh.getarrofblock()[3].setX(3);
+                sh.getarrofblock()[3].setY(20);
+                break;
+            case 2:
+                //LR SHAPE
+                sh.getarrofblock()[0].setX(3);
+                sh.getarrofblock()[0].setY(19);
+                sh.getarrofblock()[1].setX(4);
+                sh.getarrofblock()[1].setY(19);
+                sh.getarrofblock()[2].setX(5);
+                sh.getarrofblock()[2].setY(19);
+                sh.getarrofblock()[3].setX(5);
+                sh.getarrofblock()[3].setY(20);
+                break;
+            case 3:
+                //LL SHAPE
+                sh.getarrofblock()[0].setX(3);
+                sh.getarrofblock()[0].setY(19);
+                sh.getarrofblock()[1].setX(4);
+                sh.getarrofblock()[1].setY(19);
+                sh.getarrofblock()[2].setX(5);
+                sh.getarrofblock()[2].setY(19);
+                sh.getarrofblock()[3].setX(5);
+                sh.getarrofblock()[3].setY(18);
+                break;
+            case 4:
+                //T SHAPE
+                sh.getarrofblock()[0].setX(2);
+                sh.getarrofblock()[0].setY(19);
+                sh.getarrofblock()[1].setX(2);
+                sh.getarrofblock()[1].setY(20);
+                sh.getarrofblock()[2].setX(2);
+                sh.getarrofblock()[2].setY(21);
+                sh.getarrofblock()[3].setX(3);
+                sh.getarrofblock()[3].setY(20);
+                break;
+            case 5:
+                //ZL SHAPE
+                sh.getarrofblock()[0].setX(2);
+                sh.getarrofblock()[0].setY(19);
+                sh.getarrofblock()[1].setX(2);
+                sh.getarrofblock()[1].setY(20);
+                sh.getarrofblock()[2].setX(3);
+                sh.getarrofblock()[2].setY(20);
+                sh.getarrofblock()[3].setX(3);
+                sh.getarrofblock()[3].setY(21);
+                break;
+            case 6:
+                //ZR SHAPE
+                sh.getarrofblock()[0].setX(2);
+                sh.getarrofblock()[0].setY(19);
+                sh.getarrofblock()[1].setX(2);
+                sh.getarrofblock()[1].setY(20);
+                sh.getarrofblock()[2].setX(3);
+                sh.getarrofblock()[2].setY(18);
+                sh.getarrofblock()[3].setX(3);
+                sh.getarrofblock()[3].setY(19);
+                break;
+            default:
                 System.exit(0);
-            }
-            // rotate shape
-            board.insertShape(LINE); // insert shape on board
-            board.printBoard(); // print board
-            System.out.println();
+
         }
-        in.close();
+        return sh;
     }
 }
